6cab7005460d6eedf5588eae8ce0bea867bd9d2a
Eric James Michael Ritz <lobbyjones@gmail.com>
2013-07-10 03:15:34 -0400

    Add an example of switch-case formatting for the Symfony2 style
    GitHub-Issue: 111

---
+++ tests/issue-111.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Github Issue:    https://github.com/ejmr/php-mode/issues/111
+ *
+ * The code below should be formatted like so if we enable the
+ * Symfony2 style and run indent-region on it:
+ *
+ *
+ * switch ($type) {
+ * case 'order':
+ *     $clients_wants_order = $row['customer_wants_order'];
+ *     break;
+ * case 'preorder':
+ *     $clients_wants_preorder = $row['customer_wants_preorder'];
+ *     break;
+ * }
+ *
+ * The primary goal of this test is to ensure the 'case' keyword lines
+ * up with 'switch'.
+ *
+ */
+
+switch ($type) {
+    case 'order':
+        $clients_wants_order = $row['customer_wants_order'];
+        break;
+    case 'preorder':
+        $clients_wants_preorder = $row['customer_wants_preorder'];
+        break;
+}
